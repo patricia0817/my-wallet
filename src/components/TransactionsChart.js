c4cb164db63ba462a3e45db99811cf61d4281f30
amihut <amihut@EN610723>
2020-05-29 11:14:47 +0300

    populate transaction chart with data

--- src/components/TransactionsChart.js
+++ src/components/TransactionsChart.js
@@ -1,11 +1,44 @@
 import React, { useEffect, useRef, useContext } from 'react'
 import Chart from 'chart.js'
 import StateContext from '../StateContext'
+import moment from 'moment'
 
 function TransactionsChart() {
   const appState = useContext( StateContext )
 
   let ctx = useRef( null );
+
+  function getTimeLabels( today ) {
+    let count = 0;
+    let timeLabels = []
+    while ( count <= 6 ) {
+      timeLabels.push( moment( today ).subtract( count, 'days' ).format( 'dddd' ) )
+      count++;
+    }
+    return timeLabels
+  }
+
+
+  function populateTransactionData() {
+    const amountsGroupedByDate = appState.user.transactions.reduce( ( acc, tr ) => {
+      if ( acc[ tr.date ] !== undefined ) {
+        acc[ tr.date ].amount += tr.amount
+      } else {
+        acc[ tr.date ] = {
+          amount: tr.amount,
+        }
+      }
+
+      return acc
+    }, {} )
+
+    let transactionsData = [];
+
+    Object.keys( amountsGroupedByDate ).map( ( date ) => {
+      transactionsData.push( { x: date, y: amountsGroupedByDate[ date ].amount } )
+    } )
+    return transactionsData
+  }
 
 
   useEffect( () => {
@@ -13,24 +46,14 @@ function TransactionsChart() {
       new Chart( ctx.current, {
         type: 'line',
         data: {
-          labels: [ 'Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange' ],
+          labels: getTimeLabels( appState.user.transactions[ 0 ].date ),
           datasets: [ {
-            label: '# of Votes',
-            data: [ 12, 19, 3, 5, 2, 3 ],
+            label: 'Transactions',
+            data: populateTransactionData(),
             backgroundColor: [
               'rgba(255, 99, 132, 0.2)',
-              'rgba(54, 162, 235, 0.2)',
-              'rgba(255, 206, 86, 0.2)',
-              'rgba(75, 192, 192, 0.2)',
-              'rgba(153, 102, 255, 0.2)',
-              'rgba(255, 159, 64, 0.2)'
             ],
             borderColor: [
-              'rgba(255, 99, 132, 1)',
-              'rgba(54, 162, 235, 1)',
-              'rgba(255, 206, 86, 1)',
-              'rgba(75, 192, 192, 1)',
-              'rgba(153, 102, 255, 1)',
               'rgba(255, 159, 64, 1)'
             ],
             borderWidth: 1
